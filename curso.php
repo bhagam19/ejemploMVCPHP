12c25845c4a981e673e53edb7458eb6b2ad9e74b
Adolfo Ruiz <bhagam19@gmail.com>
2023-05-21 12:21:08 -0500

    Se crea el archivo

---
+++ curso.php
@@ -0,0 +1,16 @@
+<?php
+
+    $variable = 35;
+    $nombre = "Juan";
+    //bla bla bla, para comentar se utiliza el doble //   
+
+    /* Y tambien se usa la diagonal y el asterisco,*/
+    $cadena = "Mi nombre es ". $nombre ;
+
+    $colores = array("rojo", "azul", "morado"); 
+    $edad = array(12, 13, 14 ,16); //No haec falta poner comillas para los numeros
+
+    echo $cadena. "<br>" ;
+    print_r($edad). "<br>" ;
+    print_r($colores). "<br>" ;
+?>
