b9b87de59482aa650919907fda6862ffa12b8ea0
Adolfo Ruiz <bhagam19@gmail.com>
2023-05-21 12:19:30 -0500

    Se agrega el archivo

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
-<?
+<?php
+    error_reporting(E_ALL);
+    ini_set('ignore_repeat_errors', TRUE);
+    ini_set('display_errors', FALSE);
+    ini_set('log_errors', TRUE);
+    ini_set('error_log','PHPerrores.log');
+    error_log('Inicio de aplicación web');
+
     require_once 'libs/app.php';
-    $app=new App(); 
+
+    $app = new App();
 ?>
