3125df893166fe6bca3b1c60ad47fc45da6fc4af
Adolfo Ruiz <bhagam19@gmail.com>
2023-05-31 16:20:25 -0500

    Video 68

---
+++ models/loginmodel.php
@@ -0,0 +1,8 @@
+<?php   
+class LoginModel{
+
+    function __construct(){
+        
+    }
+}
+?>
---
+++ view/login/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, inicial-scale=1.0">
+    <title>Login</title>
+
+</head>
+<body>
+    <h1>Este es el Login :D </h1>
+</body>
+</html>
